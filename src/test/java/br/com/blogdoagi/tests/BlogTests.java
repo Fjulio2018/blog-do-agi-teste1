667bcbdebf45832fc32a207b916538b10a2d23ba
Francisnasc <38249727+Fjulio2018@users.noreply.github.com>
2024-05-27 13:02:30 -0300

    Primeiro teste

--- src/test/java/br/com/blogdoagi/tests/BlogTests.java
+++ src/test/java/br/com/blogdoagi/tests/BlogTests.java
@@ -3,17 +3,22 @@ package br.com.blogdoagi.tests;
 import br.com.blogdoagi.utils.hooks.Hooks;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import br.com.blogdoagi.pages.HomePage;
 
 
 
 public class BlogTests extends Hooks {
+    @BeforeTest
+    void setupTest(){
+        navegador = getDriver();
+
+    }
 
     @Test
     public void validaMenuOagibank() {
         System.out.println("Iniciando teste: validaMenuOagibank");
-        navegador = getDriver();
         HomePage homepage = new HomePage(navegador);
         Assert.assertTrue(homepage.getOagiMenu().isDisplayed(),"Não esta presente");
         Assert.assertTrue(homepage.getOagiMenu().isEnabled(),"Não esta disponivel");
